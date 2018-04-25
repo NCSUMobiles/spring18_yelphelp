e8bb07b19a11a75d9b826f99664c73dbad32dd0c
Unknown <jsmorgan142@gmail.com>
2018-04-25 13:49:22 -0400

    Added Random Number Generator

--- src/RandomPlace/RandomPlace.js
+++ src/RandomPlace/RandomPlace.js
@@ -2,20 +2,41 @@ import React from 'react';
 import {StyleSheet, View, Text, ScrollView, Picker, Button, Image, TextInput, TouchableHighlight, Dimensions, Animated,  Platform, Alert, Linking, TouchableOpacity} from 'react-native';
 //import OAuthSimple from 'oauthsimple'
 import image from '../img/yelphelp(final).png'
+import { AsyncStorage } from 'react-native'
 
 
-
+class Business {
+  constructor(){
+    this.id = 1;
+    this.name = "Dustin Business";
+    this.image_url = "https://s3-media1.fl.yelpcdn.com/bphoto/H_vQ3ElMoQ8j1bKidrv_1w/o.jpg";
+    this.is_closed = false;
+    this.url = "https://www.pizzahut.com/";
+    this.review_count = 1;
+    this.categories = ["Dustin"];
+    this.rating = "**";
+    this.coordinates = ["35.784915", "-78.690439"];
+    this.transactions = 321;
+    this.price = "$$";
+    this.location = "Raleigh";
+    this.phone = "123456789";
+    this.display_phone = "(123)-456-7890";
+    this.distance = 321.43;
+  }
+}
 
 class RandomPlace extends React.Component<ScreenProps<>> {
-		constructor() {
-		super()
-		this.state = {
-			spinValue: new Animated.Value(0)
-			responseJson: []
-			price: -1
-			radius: -1
-			term: ""
-		}
+
+
+  constructor() {
+	  super()
+		  this.state = {
+			  spinValue: new Animated.Value(0),
+			  responseJson: [],
+			  price: -1,
+			  radius: -1,
+			  term: ""
+		  }
 	}
 
 
@@ -73,7 +94,7 @@ class RandomPlace extends React.Component<ScreenProps<>> {
 	if (this.state.radius != -1) {
 		apiCall += "&radius=" + String(this.state.radius);
 	}
-	
+
 	if (this.state.type != "") {
 		apiCall += "&term=" + this.state.type;
 	}
@@ -81,16 +102,16 @@ class RandomPlace extends React.Component<ScreenProps<>> {
 		if (this.state.price == 1) {
 			apiCall += "&price=1";
 		} else if (this.state.price == 2) {
-			apiCall += "&price=1, 2";	
+			apiCall += "&price=1, 2";
 		} else if (this.state.price == 3) {
 			apiCall += "&price=1, 2, 3";
 		} else if (this.state.price == 4) {
 			apiCall += "&price=1, 2, 3, 4";
 		}
 	}
-	
+
 	apiCall += "&limit=50";
-	
+
     console.log('test');
     var yelpKey = 'VEcz4Kbd8TR68oFnT4_mdnWjRL8J5qjeN0bKCMEIPZuODihSHM_9_v-5CCJGm_QM_-kO4hx9DS9u5_5UByUATrgquPE-SeFr6VvjdMhLapg4P1jWA5Gm-gp42U-gWnYx';
 
@@ -106,11 +127,25 @@ class RandomPlace extends React.Component<ScreenProps<>> {
       .then((responseJson) => {
         this.state.responseJson = responseJson.businesses;
         console.log(responseJson.businesses);
-
+        int rand = Math.floor(Math.random() * 50);
+        $scope.business = new Business(responseJson.businesses.businesses[rand]);
+        console.log(rand);
+        AsyncStorage.setItem('business', $scope.business);
+        console.log($scope.business);
       })
       .catch((error) =>{
         console.error(error);
       });
+  }
+
+  /**
+  * Returns a random integer between min (inclusive) and max (inclusive)
+  * Using Math.round() will give you a non-uniform distribution!
+  */
+  function getRandomInt() {
+    var min = 0;
+    var max = 49;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
  	spin() {
@@ -123,6 +158,10 @@ class RandomPlace extends React.Component<ScreenProps<>> {
 		}
 		).start();
 	}
+
+  toggleSave() {
+
+  }
 
 
   render() {
