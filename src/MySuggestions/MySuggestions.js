e9182cc0c67cf5572deba08dfa99095a5743f493
Dustin <lambright.dustin@gmail.com>
2018-04-28 16:02:38 -0400

    card changes

--- src/MySuggestions/MySuggestions.js
+++ src/MySuggestions/MySuggestions.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import {StyleSheet, View, Text, ScrollView, Button, SectionList, Platform, Alert, Image, Linking, TouchableOpacity} from 'react-native';
+import {StyleSheet, View, Text, ScrollView, Button, SectionList, Platform, Alert, Image, Linking, TouchableOpacity, RefreshControl} from 'react-native';
 import { OpenMapDirections } from 'react-native-navigation-directions';
 import call from 'react-native-phone-call'
 
 //working
 class Business {
   constructor(jsonBusiness){
-    //this.id = jsonBusiness.id;
+    this.id = jsonBusiness.id;
     this.name = jsonBusiness.name;
-    // this.image_url = jsonBusiness.image_url;
-    // this.is_closed = jsonBusiness.is_closed;
-    // this.url = jsonBusiness.url;
-    // this.review_count = jsonBusiness.review_count;
-    // this.categories = jsonBusiness.categories;
-    // this.rating = jsonBusiness.rating;
-    // this.coordinates = jsonBusiness.coordinates;
-    // this.transactions = jsonBusiness.transactions;
-    // this.price = jsonBusiness.price;
-    // this.location = jsonBusiness.location;
-    // this.phone = jsonBusiness.phone;
-    // this.display_phone = jsonBusiness.display_phone;
-    // this.distance = jsonBusiness.distance;
+    this.image_url = jsonBusiness.image_url;
+    this.is_closed = jsonBusiness.is_closed;
+    this.url = jsonBusiness.url;
+    this.review_count = jsonBusiness.review_count;
+    this.categories = jsonBusiness.categories;
+    this.rating = jsonBusiness.rating;
+    this.coordinates = jsonBusiness.coordinates;
+    this.transactions = jsonBusiness.transactions;
+    this.price = jsonBusiness.price;
+    this.location = jsonBusiness.location;
+    this.phone = jsonBusiness.phone;
+    this.display_phone = jsonBusiness.display_phone;
+    this.distance = jsonBusiness.distance;
   }
 }
 
@@ -29,23 +29,14 @@ class MySuggestions extends React.Component<ScreenProps<>> {
   constructor(props){
     super(props);
     this.state = {
-      //businesses: []
-
-
+      responseJson: [],
+      price: -1,
+      radius: -1,
+      term: "",
+      businesses: [],
+      refreshing: false,
     }
-    // navigator.geolocation.getCurrentPosition(
-    //
-    //   (position) => {
-    //     this.setState({position});
-    //     this.fetchData();
-    //     console.log("newBiz: " + this.state.newBiz.id);
-    //   },
-    //
-    //   (error) => alert(error),
-    //   {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-    // );
   }
-
 
   _makeCall(businessPhone){
 
@@ -139,7 +130,7 @@ class MySuggestions extends React.Component<ScreenProps<>> {
           }
       }
 
-      apiCall += "&limit=5";
+      apiCall += "&limit=50";
       var yelpKey = 'VEcz4Kbd8TR68oFnT4_mdnWjRL8J5qjeN0bKCMEIPZuODihSHM_9_v-5CCJGm_QM_-kO4hx9DS9u5_5UByUATrgquPE-SeFr6VvjdMhLapg4P1jWA5Gm-gp42U-gWnYx';
 
 
@@ -163,10 +154,12 @@ class MySuggestions extends React.Component<ScreenProps<>> {
 
               //save every single business item into an array
               var i = 0;
-              for(i; i<5; i++){
+              for(i; i<responseJsonBusinesses.length; i++){
                   //var businessJson = responseJsonBusinesses[i];
                   var newBiz = new Business(responseJsonBusinesses[i]);
-                  alert(newBiz);
+                  // alert(newBiz.name);
+                  this.state.businesses.push(newBiz);
+                  console.log(this.state.businesses.length);
               }
 
 
@@ -181,18 +174,28 @@ class MySuggestions extends React.Component<ScreenProps<>> {
       Alert.alert(item);
     }
 
-  render() {
+    _onRefresh(){
+      this.setState({refreshing: true});
+      this.getLocationAndFetchData(); //.then(() => {this.setState({refreshing: true}));
+      this.setState({refreshing:false});
+    }
 
-    // var testBusiness = new Business();
+  render() {
 
     return (
 
       <View style={styles.listViewContainer}>
       <SectionList
         sections={[
-          {title: "MY SUGGESTIONS", data: [] }//[testBusiness, testBusiness, testBusiness, testBusiness, testBusiness]}
+          {title: "MY SUGGESTIONS", data: this.state.businesses }//[testBusiness, testBusiness, testBusiness, testBusiness, testBusiness]}
           // {title: "RESULTS", data: [testBusiness, testBusiness, testBusiness]}
         ]}
+        refreshControl={
+            <RefreshControl
+            refreshing = {this.state.refreshing}
+            onRefresh = {this._onRefresh.bind(this)}
+            />
+        }
 
         renderSectionHeader={ ({section}) => <Text style={styles.SectionHeaderStyle}> { section.title } </Text> }
         renderItem={ ({item}) =>
