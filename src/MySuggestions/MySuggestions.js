d01d22917b908a8f3007885c7f509d5dff4bd13d
dwang32 <dwang32@ncsu.edu>
2018-04-28 15:08:01 -0400

    dustin_dongyu

--- src/MySuggestions/MySuggestions.js
+++ src/MySuggestions/MySuggestions.js
@@ -6,21 +6,21 @@ import call from 'react-native-phone-call'
 //working
 class Business {
   constructor(jsonBusiness){
-    this.id = jsonBusiness.id;
+    //this.id = jsonBusiness.id;
     this.name = jsonBusiness.name;
-    this.image_url = jsonBusiness.image_url;
-    this.is_closed = jsonBusiness.is_closed;
-    this.url = jsonBusiness.url;
-    this.review_count = jsonBusiness.review_count;
-    this.categories = jsonBusiness.categories;
-    this.rating = jsonBusiness.rating;
-    this.coordinates = jsonBusiness.coordinates;
-    this.transactions = jsonBusiness.transactions;
-    this.price = jsonBusiness.price;
-    this.location = jsonBusiness.location;
-    this.phone = jsonBusiness.phone;
-    this.display_phone = jsonBusiness.display_phone;
-    this.distance = jsonBusiness.distance;
+    // this.image_url = jsonBusiness.image_url;
+    // this.is_closed = jsonBusiness.is_closed;
+    // this.url = jsonBusiness.url;
+    // this.review_count = jsonBusiness.review_count;
+    // this.categories = jsonBusiness.categories;
+    // this.rating = jsonBusiness.rating;
+    // this.coordinates = jsonBusiness.coordinates;
+    // this.transactions = jsonBusiness.transactions;
+    // this.price = jsonBusiness.price;
+    // this.location = jsonBusiness.location;
+    // this.phone = jsonBusiness.phone;
+    // this.display_phone = jsonBusiness.display_phone;
+    // this.distance = jsonBusiness.distance;
   }
 }
 
@@ -29,8 +29,10 @@ class MySuggestions extends React.Component<ScreenProps<>> {
   constructor(props){
     super(props);
     this.state = {
-      businesses: []
-		}
+      //businesses: []
+
+
+    }
     // navigator.geolocation.getCurrentPosition(
     //
     //   (position) => {
@@ -94,51 +96,85 @@ class MySuggestions extends React.Component<ScreenProps<>> {
   componentDidMount() {
     var businesses = [];
     this.setState(businesses);
-    // navigator.geolocation.getCurrentPosition(
-    //
-    //   (position) => {
-    //     this.setState({position});
-    //     this.fetchData();
-    //   },
-    //
-    //   (error) => alert(error),
-    //   {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-
-
-    // businessArray = getDataFromDisk()
-    //)
+    this.getLocationAndFetchData();
   };
 
+  getLocationAndFetchData(){
+      navigator.geolocation.getCurrentPosition(
+          (position) => {
+              this.setState({position});
+              console.log("fetching data");
+              this.fetchData();
+          },
+          (error) => alert(error),
+          {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+      );
+  }
+
+
   fetchData() {
-    var lat = this.state.position.coords.latitude
-    var lng = this.state.position.coords.longitude
-    var latstr = "latitude=" + String(lat) + "&"
-    var lngstr = "longitude=" + String(lng) + "&"
+      var apiCall = "https://api.yelp.com/v3/businesses/search?";
+      var lat = this.state.position.coords.latitude;
+      var lng = this.state.position.coords.longitude;
+      apiCall += "latitude=" + String(lat);
+      apiCall += "&longitude=" + String(lng);
 
-    console.log('test');
-    var yelpKey = 'VEcz4Kbd8TR68oFnT4_mdnWjRL8J5qjeN0bKCMEIPZuODihSHM_9_v-5CCJGm_QM_-kO4hx9DS9u5_5UByUATrgquPE-SeFr6VvjdMhLapg4P1jWA5Gm-gp42U-gWnYx';
+      //Radius
+      if (this.state.radius != -1) {
+          apiCall += "&radius=" + String(this.state.radius);
+      }
+
+      if (this.state.term != "") {
+          apiCall += "&term=" + this.state.term;
+      }
+      if (this.state.price != -1) {
+          if (this.state.price == 1) {
+              apiCall += "&price=1";
+          } else if (this.state.price == 2) {
+              apiCall += "&price=1, 2";
+          } else if (this.state.price == 3) {
+              apiCall += "&price=1, 2, 3";
+          } else if (this.state.price == 4) {
+              apiCall += "&price=1, 2, 3, 4";
+          }
+      }
+
+      apiCall += "&limit=5";
+      var yelpKey = 'VEcz4Kbd8TR68oFnT4_mdnWjRL8J5qjeN0bKCMEIPZuODihSHM_9_v-5CCJGm_QM_-kO4hx9DS9u5_5UByUATrgquPE-SeFr6VvjdMhLapg4P1jWA5Gm-gp42U-gWnYx';
 
 
-    fetch('https://api.yelp.com/v3/businesses/search?term=food&latitude=35.7796&longitude=-78.6382&limit=50', {
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer ' + yelpKey,
-      },
-      body: undefined,
-    })
-      .then((response) => response.json())
-      .then((responseJson) => {
-        for (let o of responseJson.businesses) {
-          var newBiz = new Business(o);
-          this.setState(newBiz);
-          console.log(newBiz.rating);
-
-        }
-
+      fetch(apiCall, {
+          method: 'GET',
+          headers: {
+              Authorization: 'Bearer ' + yelpKey,
+          },
+          body: undefined,
       })
-      .catch((error) =>{
-        console.error(error);
-      });
+          .then((response) => response.json())
+          .then((responseJson) => {
+              var responseJsonBusinesses = responseJson.businesses;
+              //var arrayLength = responseJsonBusinesses.length;
+              // var selectedBusiness = Math.floor((Math.random() * arrayLength) + 1);
+              // console.log("selected business " + selectedBusiness);
+              // var newBiz = new Business(responseJsonBusinesses[selectedBusiness]);
+              // console.log(newBiz.name);
+              // this.state.selectedBusiness = newBiz;
+              // console.log(this.state.selectedBusiness.name);
+
+              //save every single business item into an array
+              var i = 0;
+              for(i; i<5; i++){
+                  //var businessJson = responseJsonBusinesses[i];
+                  var newBiz = new Business(responseJsonBusinesses[i]);
+                  alert(newBiz);
+              }
+
+
+
+          })
+          .catch((error) =>{
+              console.error(error);
+          });
   }
 
   GetSectionListItem=(item)=>{
@@ -154,7 +190,7 @@ class MySuggestions extends React.Component<ScreenProps<>> {
       <View style={styles.listViewContainer}>
       <SectionList
         sections={[
-          {title: "MY SUGGESTIONS", data: this.state.businesses}//[testBusiness, testBusiness, testBusiness, testBusiness, testBusiness]}
+          {title: "MY SUGGESTIONS", data: [] }//[testBusiness, testBusiness, testBusiness, testBusiness, testBusiness]}
           // {title: "RESULTS", data: [testBusiness, testBusiness, testBusiness]}
         ]}
 
